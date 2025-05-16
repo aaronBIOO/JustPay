05bac1b3cbd34de5850e88baab4171fd020065f4
aaronBIOO <aaronnabioo@gmail.com>
2025-05-16 19:43:34 +0000

    history page structure

---
+++ src/app/components/HistoryContent.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+function HistoryContent() {
+  return (
+    <div className="w-full">
+      {/* Content will be added here */}
+    </div>
+  );
+}
+
+export default HistoryContent;
